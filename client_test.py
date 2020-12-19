4cb20bcdac646da6a74a5d2ef3f406868ffcb3d4
Molena (Thy) Hoang-Xuan Nguyen <molenathyhoangxuannguyen@users.noreply.github.com>
2020-12-19 16:36:45 -0500

    First Task completed!

--- client_test.py
+++ client_test.py
@@ -9,12 +9,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-    # The following are the test cases for this
+     #The following are the test cases for this
     self.assertEqual(getDataPoint(quotes[0]),('ABC',120.48,121.2,120.84))
-    # #quotes[0] is actually the quote that we need
+    #quotes[0] is actually the quote that we need
     self.assertEqual(getDataPoint(quotes[1]),('DEF',117.87,121.68,119.775))
     self.assertEqual(getRatio(1,1),1)
-    # self.assertEqual(getRatio(100/20),5)
+    #self.assertEqual(getRatio(100/20),5)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -22,18 +22,13 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-    # The following are the test cases 
+
+    """ ------------ Add more unit tests ------------ """
+  # The following are the test cases 
     data_point_1 = getDataPoint(quotes[0])
     self.assertGreater(data_point_1[1],data_point_1[2])
-
     data_point_2 = getDataPoint(quotes[1])
-    self.assertGreater(data_point_1[1],data_point_1[2])
-
-
-
-
-
-
+    self.assertGreater(data_point_2[2],data_point_2[3])
 
 
 
